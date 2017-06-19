0761fc91451a2c67353adc49778e313fcf526fb5
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-06-19 22:42:48 +0200

    Sait tout faire

---
+++ montre-la-note.js
@@ -0,0 +1,21 @@
+const {Afficheur} = require('./domaine/afficheur')
+const {traduit} = require('./domaine/traducteur')
+
+function main() {
+    const nomDeNote = process.argv[2]
+    const note = traduit(nomDeNote)
+
+    if (!note) {
+        console.log(`Note ${nomDeNote} non trouvée.`)
+        process.exit(1)
+    }
+
+    const afficheur = new Afficheur(console, note)
+
+    afficheur.log()
+}
+
+if (require.main === module) {
+    main();
+}
+
